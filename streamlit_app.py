52f81f8a2178aeeb74d10f750cc1fed97fb4ed6c
Ramalingam Sengoden <115408922+ramsengodenlingam@users.noreply.github.com>
2022-10-09 10:05:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parenets New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach @ Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-range Egg')
