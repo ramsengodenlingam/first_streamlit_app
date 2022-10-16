a832aae6185a22830044b58da3da31b812c964ea
Ramalingam Sengoden <115408922+ramsengodenlingam@users.noreply.github.com>
2022-10-16 13:00:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,17 +44,20 @@ try:
 except URLError as e:
    streamlit.error()
   
-streamlit.stop()
 #SNOWFLAKE CONNECTION
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("fruit_load_list contains:")
-streamlit.dataframe(my_data_row)
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * From pc_rivery_db.public.fruit_load_list")
+      return my_cur.fetchall()
 
-add_my_fruit = streamlit.text_input('what fruit you would like to add','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+if streamlit.button('Get fruit load list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
+#add_my_fruit = streamlit.text_input('what fruit you would like to add','jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
+
+streamlit.stop()
 #This will not work correctly but just go with it for now
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
