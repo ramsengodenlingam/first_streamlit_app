2fcaa38cb893efcfa7f9b2adc0dd6c7742170a27
Ramalingam Sengoden <115408922+ramsengodenlingam@users.noreply.github.com>
2022-10-15 21:37:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parenets New Healthy Dinner')
 streamlit.header('Breakfast Menu')
@@ -19,3 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 #streamlit.dataframe(my_fruit_list)
+
+#New section to display fruitvise response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
